9f2879e6e0bfa14c09205dcedc5dc3fe8999c847
sudin manandhar <smanandhar@bitscrafters.com>
2016-08-03 16:42:08 +0545

    controller modified

--- laravel-filemanager/src/controllers/Controller.php
+++ laravel-filemanager/src/controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php namespace Unisharp\Laravelfilemanager\controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Bus\DispatchesCommands as DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
